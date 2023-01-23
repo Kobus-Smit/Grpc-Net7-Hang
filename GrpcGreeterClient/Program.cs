d3a4dbae5b4d48ef34a8ac7735e04747d5f6831c
Kobus-Smit <ksmit.aus@gmail.com>
2023-01-23 14:21:42 +1100

    Add trace logging as per https://learn.microsoft.com/en-us/aspnet/core/grpc/diagnostics?view=aspnetcore-7.0

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System.Threading.Tasks;
 using Grpc.Net.Client;
 using GrpcGreeterClient;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+var loggerFactory = LoggerFactory.Create(logging =>
+{
+    logging.SetMinimumLevel(LogLevel.Trace);
+    logging.AddSimpleConsole(options =>
+    {
+        options.TimestampFormat = "HH:mm:ss.fff ";
+        options.ColorBehavior = LoggerColorBehavior.Enabled;
+    });
+});
 
 // The port number must match the port of the gRPC server.
-using var channel = GrpcChannel.ForAddress("https://KS:40125");
+using var channel = GrpcChannel.ForAddress("https://KS:40125",
+    new GrpcChannelOptions { LoggerFactory = loggerFactory  });
 var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(
-    new HelloRequest { Name = "GreeterClient" });
-Console.WriteLine("Greeting: " + reply.Message);
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
+
+while (true)
+{
+    var reply = await client.SayHelloAsync(
+        new HelloRequest { Name = "GreeterClient" });
+    Console.WriteLine("Greeting: " + reply.Message);
+    Console.WriteLine("Press any key to exit...\n");
+    Console.ReadKey();
+}
--- GrpcService1/Program.cs
+++ GrpcService1/Program.cs
@@ -1,6 +1,15 @@
 using GrpcService1.Services;
+using Microsoft.Extensions.Logging.Console;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging
+    .ClearProviders()
+    .SetMinimumLevel(LogLevel.Trace)
+    .AddSimpleConsole(options =>
+    {
+        options.TimestampFormat = "HH:mm:ss.fff ";
+        options.ColorBehavior = LoggerColorBehavior.Enabled;
+    });
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
@@ -9,7 +18,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
